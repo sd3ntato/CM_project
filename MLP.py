a7266213e725e0c865e84cf3daf197b2804ed0b8
sd3ntato <valeriomariani52@gmail.com>
2021-03-11 12:38:42 +0100

    prima versione classical momentum

--- MLP.py
+++ MLP.py
@@ -5,7 +5,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
-#from IPython.display import clear_output
+from IPython.display import clear_output
 
 # activation functions
 from numpy import tanh
@@ -13,33 +13,6 @@ from scipy.special import softmax
 ide = lambda x : np.copy(x)
 relu = lambda x: x*(x > 0)
 #softmax = lambda x: np.exp(x - logsumexp(x, keepdims=True)) # implementazione scipy special
-
-def to_categorical(y, num_classes=None, dtype='float32'): # code from keras implementation: keras.utils.to_categorical
-  """Converts a class vector (integers) to binary class matrix.
-  E.g. for use with categorical_crossentropy.
-  Args:
-      y: class vector to be converted into a matrix
-          (integers from 0 to num_classes).
-      num_classes: total number of classes. If `None`, this would be inferred
-        as the (largest number in `y`) + 1.
-      dtype: The data type expected by the input. Default: `'float32'`.
-  Returns:
-      A binary matrix representation of the input. The classes axis is placed
-      last.
-  """
-  y = np.array(y, dtype='int')
-  input_shape = y.shape
-  if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
-    input_shape = tuple(input_shape[:-1])
-  y = y.ravel()
-  if not num_classes:
-    num_classes = np.max(y) + 1
-  n = y.shape[0]
-  categorical = np.zeros((n, num_classes), dtype=dtype)
-  categorical[np.arange(n), y] = 1
-  output_shape = input_shape + (num_classes,)
-  categorical = np.reshape(categorical, output_shape)
-  return categorical
 
 # loss functions:
 squared_error = lambda y,d:  np.linalg.norm(y - d) ** 2 # categorical cross-entropy
@@ -59,7 +32,7 @@ def derivative(f):
   elif f == ide or f == softmax:
     return lambda x : x-x+1
   elif f == squared_error or f == cross_entropy:
-    return lambda d,y: d-y
+    return lambda d,y: y-d 
 
 def get_f(f):
   """
@@ -179,7 +152,7 @@ class MLP():
 
     return d
 
-  def compute_gradient(self,p): 
+  def compute_gradient_p(self,p): 
     """
     compute gradient over pattern p
     """
@@ -199,84 +172,35 @@ class MLP():
 
     return np.array(grad)
 
-  def epoch_batch_BP(self, train_x:np.ndarray, train_y:np.ndarray, eta, a=1e-12,l=1e-12):
+  def momentum_train(self, train_x:np.ndarray, train_y:np.ndarray, epsilon, mu, max_epochs=300 ):
     """
-    Use all patterns in the given training set to execute an epoch of batch training with (possibly) thickonov regularization
-    train_x : input in training set
-    train_y : output in training set
-    eta: learning rate
-    a: momentum rate
-    l: thickonov regularization rate
+      trains the network using classical momentum
     """
-    old_deltas = np.array( [ np.zeros(self.w[i].shape) for i in range(self.Nl+1) ] ,dtype=object) # prevous delta for momentum computation
-    p = np.array( [ np.zeros(self.w[i].shape) for i in range(self.Nl+1) ] ,dtype=object) # partial sum of gradients, in here i accumulate sum of gradients computed on each pattern
-    N = np.size(train_x,axis=0) # number of patterns in training set
+    # number of patterns in training set
+    N = np.size(train_x,axis=0) 
+    epoch = 0
 
-    # compute gradient summing over partial gradients
-    comp_grad = self.compute_gradient
-    p = sum( map( comp_grad, zip( train_x,train_y ) ) )/N
+    # functions for gradient computation
+    compute_partial_gradient = self.compute_gradient_p # function for computing paryial gradient gradient on pattern on pattern p
+    compute_gradient = lambda train_x, train_y: sum( map( compute_partial_gradient, zip( train_x,train_y ) ) )/N # function for computing gradient of loss function over all patterns
 
-    #compute deltas
-    deltas = ( eta * p - l * self.w ) + a * old_deltas
+    # prevous velocity (v_t) for momentum computation
+    old_v = np.array( [ np.zeros(self.w[i].shape) for i in range(self.Nl+1) ] ,dtype=object) 
 
-    # update weights and old_deltas values
-    self.w += deltas
-    old_deltas = deltas
-  
-  def train(self, train_x, train_y, eta, a=1e-12, l=1e-12, val_x=None, val_y=None, max_epochs=300, tresh=.01, mode='batch', shuffle_data=True, measure_interval=10, verbose=True):
-    """
-    Executes a maximum of max_epochs epochs of training using the function epoch_f in order to do regression of some function that maps input train_x->train_y.
-    After each measure_interval epochs, mesures error on training set, and exits when training error falls below given treshold.
-    Returns error at each mesurement calculated both on training and validation set, so you can plot them.
-    Could use some early stopping mechanism through validation error.
-    """
-    e = [None]*max_epochs  # array of training errors for each epoch
-    v = [None]*max_epochs # array of validation errors for each epoch
+    # si deve ferma quando gradiente piccolo ma gradiente e' un tensore
+    while( epoch < max_epochs ):
+      # compute gradient ( \Nabla l(w_t) )
+      g = compute_gradient( train_x,train_y )
+
+      #compute velocity v_{t+1}
+      v = mu * old_v - epsilon * g
+
+      # update weights and prevoius velocity
+      self. w = self.w + v
+      old_v = v
+
+      epoch +=1
     
-    # set function that does training epoch
-    epoch_f=None 
-    if mode=='batch':
-      epoch_f = self.epoch_batch_BP
-    
-    # execute epochs of training until training is complete or max_epochs epochs are executed (or training error diverges)
-    for i in range(max_epochs):
-
-      # shuffle training set if needed
-      if shuffle_data==True:
-        train_x, train_y = shuffle(train_x, train_y)
-      
-      # execute an epoch of training
-      epoch_f( train_x, train_y, eta, a=a, l=l ) # epoca di allenamento
-      
-      # after each measure_interval epochs of training do calculation:
-      # decide if training is done and mesure training and validation error
-      if i % measure_interval == 0:
-        idx_m = int(i/measure_interval) # number of mesurements done 
-
-        # mesure error on validation set if validation set is provided
-        if val_x is not None:
-          outs_v = self.supply_sequence( val_x ) # actual outputs of the network on validation set
-          if outs_v.shape != val_y.shape:
-            outs_v = outs_v.reshape(val_y.shape) # reshape when needed or error calculation doesn't work
-          assert outs_v.shape == val_y.shape
-          v[idx_m] = self.error(outs_v,val_y) # Mean Squared Error on training set
-
-        # measure error on training set to decide if training is complete
-        outs_t = self.supply_sequence( train_x ) # actual outputs of the network on training set
-        if outs_t.shape != train_y.shape:
-          outs_t = outs_t.reshape(train_y.shape) # reshape when neede or error calculation doesn't work
-        assert outs_t.shape == train_y.shape
-        e[idx_m] = self.error(outs_t,train_y) # error on training set
-        if verbose: print(f'training error atm: {e[idx_m]}') 
-        
-        # if training is complete exit the loop. training is complete when training error falls below treshold tresh or 
-        # error on training set is getting worse due to bad training parameters
-        if i>0 and ( e[idx_m] < tresh or e[idx_m] > e[idx_m-1]):  # we quit training when error on training set falls below treshold
-          if verbose: print(f'final error: {e[idx_m]}')
-          break
-        # clear_output(wait=True)
-
-    return e, v
 
   def supply(self, u):
     """
@@ -310,72 +234,4 @@ class MLP():
   def test(self, X, Y):
     outs = self.supply_sequence(X)
     return self.error(outs, Y)
-  
-  """
-  alla fine questa non e' che serva davvero.....
-  def classify(self,u):
-    classify sample. To be written properly
-    u: imput pattern
-    if self.f_out == softmax:
-      return -1 if float( self.supply(u) )<0 else 1
-  """
 
-"""
-  questa andrebbe riscritta bellina come quella batch
-  def epoch_online_BP(self, train_x:np.ndarray, train_y:np.ndarray, eta):
-    ""
-    Use all patterns in the given training set to execute an epoch of online training
-    train_x : input in training set
-    train_y : output in training set
-    eta: learning rate
-    ""
-    for i in range(np.size(train_x,axis=0)):
-      
-      self.compute_gradient() 
-
-      for m in range(self.Nl+1):
-        self.w[m] += eta * self.grad[m] 
-"""
-
-"""
-def score(o,d): 
-  percentage score of nework in binary classification task. To be written properly
-  o: sequence of ouputs of the network, 
-  d: sequence of desired outputs
-  return ( o - d == 0).sum()/len(o)*100
-"""
-
-"""
-from itertools import product  
-def combinations(Nh,Nls):
-    x=[]
-    for Nl in Nls :
-        x.append(list(permutations(Nh_monk,Nls_monk)))
-    
-    combinedList=[*x[0],*x[1],*x[2]]
-    return combinedList
-
-Nh = [2, 3, 4]
-Nls = [2, 3]
-
-grid = {
-  'units' : combinations(Nh,Nls),
-  'lr' : [1e-01, 1e-02, 1e-03],  # learning rate
-  'a' : [1e-01, 1e-02, 1e-03, 0],  # momento
-  'l' : [1e-01, 1e-02, 1e-03, 1e-10, 1e-12, 0],  # regolarizzazione
-  'f' : ['relu','tanh'], # funzione attivazione
-  'w_init' : [0.7, 1e-02], # scalining iniziale pesi
-}
-
-def k_fold_CV(train_x, train_y, k, n_init, grid):
-  # splittare il training set in k fold
-  splits = split( train_x, train_y)  
-
-  # per ogni configurazione in grid:
-      # per ogni split in splits:
-        # inizializza n_init reti con configurazione
-        # allena
-        # testa
-        # salva la migliore configurazione
-
-"""
